f9e3a21dfe12ab81933b93fbeddf15f3902ba333
KMSkelton <kmskelton@outlook.com>
2015-05-20 16:16:05 -0700

    To do app: can add, delete, need edit

---
+++ app.js
@@ -0,0 +1,39 @@
+// Build a Todo app using HTML, CSS and jQuery. 
+// Your to-do app should display a list of to-do items as well as add, edit and delete them. 
+// Feel free to add to anything you’d like to that application
+$("#new-text").keypress(function(e) {
+	if (e.which == 13) { //press enter key
+		$("#add").click(); //trigger click event
+		return false;
+	}
+});
+$("#add").on("click", function() {
+	var text = $("#new-text").val();
+
+	if ($.trim($("#new-text").val()) === "") { //no empty list items
+		event.preventDefault();
+		alert("Please add an item");
+	} else {
+		$("#todoList").append("<li><input type='checkbox' class='done' />" + text + " <button class='delete'>Delete</button> <button class='edit'>Edit</button></li>");
+		$("#new-text").val("");
+	}
+
+});
+
+
+$(document).on("click", ".delete", function() { //delete items 
+	$(this).parent().remove();
+});
+
+$(document).on("click", ".edit", function() {
+	$(this).parent().append();
+});
+
+$(document).on("click", ".done", function() { //line through completed items
+	if ($(this).parent().css("textDecoration") === "line-through") {
+		$(this).parent().css("textDecoration", "none");
+	} else {
+		$(this).parent().css("textDecoration", "line-through");
+	}
+});
+
